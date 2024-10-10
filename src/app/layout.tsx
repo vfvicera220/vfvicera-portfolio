f9334c08a72e37099259c7c58a37db1224f148b8
vfvicera220 <vfvicera220@gmail.com>
2024-10-10 17:19:32 +0800

    Add vercel analytics.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         className={`${montserrat.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
